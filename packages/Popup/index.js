289d73b0c861d1705b594e20300f6547bc3c1bda
c10342 <you@example.com>
2020-10-21 23:04:35 +0800

    feat：添加Popup弹出层组件

---
+++ packages/Popup/index.js
@@ -0,0 +1,60 @@
+import { transition } from "../behaviors/transition";
+Component({
+  behaviors: [transition(false)],
+  properties: {
+    zIndex: {
+      type: Number,
+      value: 100,
+    },
+    mask: {
+      type: Boolean,
+      value: true,
+    },
+    maskStyle: String,
+    position: {
+      type: String,
+      value: "center",
+      observer: "observeClass",
+    },
+    closeable: {
+      type: Boolean,
+      value: false,
+    },
+    closeIcon: { type: String, value: "close" },
+    closeIconPosition: {
+      type: String,
+      value: "top-right",
+    },
+    closeOnClickMask: {
+      type: Boolean,
+      value: true,
+    },
+    closeIconSize: {
+      type: String,
+      value: "40rpx",
+    },
+  },
+  data: {},
+  methods: {
+    onMaskClick() {
+      this.triggerEvent("mask-click");
+      if (this.properties.closeOnClickMask) {
+        this.triggerEvent("close");
+      }
+    },
+    observeClass() {
+      const { position } = this.properties;
+      this.setData({ name: position });
+    },
+    onCloseClick() {
+      this.triggerEvent("close");
+    },
+  },
+  created: function () {
+    this.observeClass();
+  },
+  attached: function () {},
+  ready: function () {},
+  moved: function () {},
+  detached: function () {},
+});
