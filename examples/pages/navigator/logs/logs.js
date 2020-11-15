4ed007d4e458573bd030878a73434f340d4072f6
c10342 <1376023740@qq.com>
2020-11-15 12:50:45 +0800

    feat：更新日志

--- examples/pages/navigator/logs/logs.js
+++ examples/pages/navigator/logs/logs.js
@@ -1,57 +1,31 @@
 export default [{
-    time: '2020-10-1',
+    time: '2020-11-15',
     messageList: [
-      '新增 Button 按钮组件',
-      '新增 Icon 图标组件'
+      '新增 Stepper 步进器组件'
     ]
   },
   {
-    time: '2020-10-3',
+    time: '2020-11-12',
     messageList: [
-      '新增 Image 图片组件'
+      '新增 Slider 滑块组件'
     ]
   },
   {
-    time: '2020-10-6',
+    time: '2020-11-09',
     messageList: [
-      '新增 Mask 遮罩层组件',
-      '新增 Transition 动画组件'
+      '新增 Search 搜索组件'
     ]
   },
   {
-    time: '2020-10-10',
+    time: '2020-11-07',
     messageList: [
-      '新增 Layout 布局组件'
+      '新增 Rate 评分组件'
     ]
   },
   {
-    time: '2020-10-13',
+    time: '2020-11-05',
     messageList: [
-      '新增 Loading 加载组件'
-    ]
-  },
-  {
-    time: '2020-10-18',
-    messageList: [
-      '新增 Cell 单元格组件'
-    ]
-  },
-  {
-    time: '2020-10-21',
-    messageList: [
-      '新增 Popup 弹出层组件'
-    ]
-  },
-  {
-    time: '2020-10-24',
-    messageList: [
-      '新增 Calendar 日历组件'
-    ]
-  },
-  {
-    time: '2020-10-25',
-    messageList: [
-      '新增 Checkbox 复选框组件'
+      '新增 Radio 单选框组件'
     ]
   },
   {
@@ -61,15 +35,59 @@ export default [{
     ]
   },
   {
-    time: '2020-11-5',
+    time: '2020-10-25',
     messageList: [
-      '新增 Radio 单选框组件'
+      '新增 Checkbox 复选框组件'
     ]
   },
   {
-    time: '2020-11-7',
+    time: '2020-10-24',
     messageList: [
-      '新增 Rate 评分组件'
+      '新增 Calendar 日历组件'
+    ]
+  },
+  {
+    time: '2020-10-21',
+    messageList: [
+      '新增 Popup 弹出层组件'
+    ]
+  },
+  {
+    time: '2020-10-18',
+    messageList: [
+      '新增 Cell 单元格组件'
+    ]
+  },
+  {
+    time: '2020-10-13',
+    messageList: [
+      '新增 Loading 加载组件'
+    ]
+  },
+  {
+    time: '2020-10-10',
+    messageList: [
+      '新增 Layout 布局组件'
+    ]
+  },
+  {
+    time: '2020-10-06',
+    messageList: [
+      '新增 Mask 遮罩层组件',
+      '新增 Transition 动画组件'
+    ]
+  },
+  {
+    time: '2020-10-03',
+    messageList: [
+      '新增 Image 图片组件'
+    ]
+  },
+  {
+    time: '2020-10-01',
+    messageList: [
+      '新增 Button 按钮组件',
+      '新增 Icon 图标组件'
     ]
   },
 ]
