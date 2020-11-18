2f353b226e6f7ee64bbed3ed81a805bd3063249a
c10342 <1376023740@qq.com>
2020-11-18 23:23:44 +0800

    feat：完成Picker组件部分功能

--- packages/Picker/index.js
+++ packages/Picker/index.js
@@ -9,14 +9,57 @@ Component({
       type: Array,
       value: [],
     },
+    itemHeight: {
+      type: Number,
+      value: 44,
+    },
+    valueKey: {
+      type: String,
+      value: "text",
+    },
+    showToolbar: Boolean,
+    title: String,
+    confirmButtonText: {
+      type: String,
+      value: "确认",
+    },
+    cancelButtonText: {
+      type: String,
+      value: "取消",
+    },
+    toolbarPosition: {
+      type: String,
+      value: "top",
+      options: ["top", "bottom"],
+    },
+    visibleItemCount: {
+      type: Number,
+      value: 6,
+      observer: "updateTopVisible",
+    },
   },
-  data: {},
+  data: {
+    topVisible: 2,
+  },
   methods: {
-    onTouchMove() {},
+    updateTopVisible() {
+      const { visibleItemCount } = this.properties;
+      const topVisible = parseInt(visibleItemCount / 2) - 1;
+      this.setData({
+        topVisible,
+      });
+    },
+    onChange(data) {
+      this.triggerEvent("change", data.detail);
+    },
+    onCancel() {},
+    onConfirm() {},
   },
   created: function() {},
   attached: function() {},
-  ready: function() {},
+  ready: function() {
+    this.updateTopVisible();
+  },
   moved: function() {},
   detached: function() {},
 });
