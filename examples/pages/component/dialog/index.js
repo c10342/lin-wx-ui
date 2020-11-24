05dd9c067cf9ea6d653a6f23c1967893993af9dc
c10342 <1376023740@qq.com>
2020-11-24 22:28:44 +0800

    feat：更新演示小程序

--- examples/pages/component/dialog/index.js
+++ examples/pages/component/dialog/index.js
@@ -1,65 +1,125 @@
+import Dialog from '../../../dist/Dialog/dialog.js';
 
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-    },
-  
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-  
-    },
-  
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-  
-    },
-  
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-  
-    },
-  
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-  
-    },
-  
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-  
-    },
-  
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-  
-    },
-  
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-  
-    },
-  
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-  
-    },
-  })
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    show:false
+  },
+
+  onClick1() {
+    Dialog.alert({
+      title: '标题',
+      message: '弹窗内容'
+    }).then(() => {});
+  },
+  onClick2() {
+    Dialog.alert({
+      message: '弹窗内容',
+    }).then(() => {});
+  },
+  onClick3() {
+    Dialog.confirm({
+      title: '标题',
+      message: '弹窗内容',
+    }).then(() => {}).catch(() => {});
+  },
+  onClick4() {
+    Dialog.alert({
+      title: '标题',
+      message: '弹窗内容',
+      theme: 'round-button',
+    }).then(() => {
+      // on close
+    });
+  },
+  onClick5() {
+    Dialog.alert({
+      message: '弹窗内容',
+      theme: 'round-button',
+    }).then(() => {
+      // on close
+    });
+  },
+  onClick6() {
+    Dialog.confirm({
+        title: '标题',
+        message: '弹窗内容',
+        asyncClose: true
+      })
+      .then(() => {
+        setTimeout(() => {
+          Dialog.close();
+        }, 1000);
+      })
+      .catch(() => {
+        Dialog.close();
+      });
+  },
+  onClick7(){
+    this.setData({
+      show:true
+    })
+  },
+  onClose() {
+    this.setData({ show: false });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+})
