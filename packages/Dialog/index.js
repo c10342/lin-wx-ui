80872c9f0e76b317e652d88c1a09597ac95fe6df
c10342 <1376023740@qq.com>
2020-11-24 22:38:40 +0800

    feat：完成Dialog弹框组件

--- packages/Dialog/index.js
+++ packages/Dialog/index.js
@@ -1,4 +1,6 @@
 import { RED, GRAY } from "../common/color";
+import { button } from "../behaviors/button";
+import { openType } from "../behaviors/open-type";
 
 Component({
   options: {
@@ -6,9 +8,15 @@ Component({
     multipleSlots: true,
   },
   externalClasses: ["custom-class"],
+  behaviors: [button, openType],
   properties: {
     show: {
       type: Boolean,
+      observer(show) {
+        if (!show) {
+          this.stopLoading();
+        }
+      },
     },
     title: String,
     message: String,
@@ -61,13 +69,75 @@ Component({
       type: Boolean,
       value: true,
     },
-    // transition: {
-    //   type: String,
-    //   value: "scale",
-    // },
+    transition: {
+      type: String,
+      value: "scale",
+    },
   },
-  data: {},
-  methods: {},
+  data: {
+    onConfirm: null,
+    onCancel: null,
+    loading: {
+      confirm: false,
+      cancel: false,
+    },
+  },
+  methods: {
+    onMaskClick() {
+      this.onClose("mask");
+    },
+    onConfirm() {
+      if (this.data.loading.confirm) {
+        return;
+      }
+      this.handleAction("confirm");
+    },
+    onCancel() {
+      if (this.data.loading.cancel) {
+        return;
+      }
+      this.handleAction("cancel");
+    },
+    handleAction(action) {
+      if (this.data.asyncClose) {
+        this.setData({
+          [`loading.${action}`]: true,
+        });
+      }
+      this.onClose(action);
+    },
+    onCancel(action) {
+      this.onClose(action);
+    },
+    close() {
+      this.setData({
+        show: false,
+      });
+    },
+    stopLoading() {
+      this.setData({
+        loading: {
+          confirm: false,
+          cancel: false,
+        },
+      });
+    },
+    onClose(action) {
+      if (!this.properties.asyncClose) {
+        this.close();
+      }
+      this.triggerEvent("close", action);
+      this.triggerEvent(action, {
+        dialog: this,
+      });
+      const callback = this.data[
+        action === "confirm" ? "onConfirm" : "onCancel"
+      ];
+      if (callback) {
+        callback(this);
+      }
+    },
+  },
   created: function() {},
   attached: function() {},
   ready: function() {},
