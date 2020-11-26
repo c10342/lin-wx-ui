c5598b309d9fa318e435d310ecdba42d02e87ab6
c10342 <1376023740@qq.com>
2020-11-26 21:59:09 +0800

    feat：完成DrapdownMenu下拉菜单功能

--- packages/DropdownItem/index.js
+++ packages/DropdownItem/index.js
@@ -1,4 +1,5 @@
-//Component Object
+import { RED } from "../common/color";
+import defaultProps from "../DropdownMenu/props";
 Component({
   relations: {
     "../DropdownMenu/index": {
@@ -6,7 +7,6 @@ Component({
       linked(parent) {
         this.parent = parent;
         this.updateDataFromParent();
-        this.setWrapperStyle();
       },
       unlinked() {
         this.parent = null;
@@ -36,16 +36,106 @@ Component({
   },
   data: {
     wrapperStyle: "",
+    showPopup: false,
+    showWrapper: false,
+    transition: false,
+    activeColor: defaultProps.activeColor,
+    duration: defaultProps.duration,
+    direction: defaultProps.direction,
+    mask: defaultProps.mask,
+    closeOnClickMask: defaultProps.closeOnClickMask,
   },
   methods: {
-    updateDataFromParent() {},
-    setWrapperStyle() {
+    onOptionTap(event) {
+      const option = event.currentTarget.dataset.option;
+      const { value } = option;
+      const shouldEmitChange = this.properties.value != value;
+      this.triggerEvent("close");
+      this.setData({
+        transition: true,
+        showPopup: false,
+      });
+      this.rerender();
+      if (shouldEmitChange) {
+        this.triggerEvent("change", value);
+      }
+    },
+    onOpen() {
+      this.triggerEvent("open");
+    },
+    onClose() {
+      this.triggerEvent("close");
+    },
+    onOpened() {
+      this.triggerEvent("opended");
+    },
+    onClosed() {
+      this.triggerEvent("closed");
+      this.setData({ showWrapper: false, showPopup: false });
+    },
+    rerender() {
+      wx.nextTick(() => {
+        if (this.parent) {
+          this.parent.updateItemListData();
+        }
+      });
+    },
+    onMaskClose() {
+      this.toggle();
+    },
+    toggle(immediate = false) {
+      const { showPopup } = this.data;
+      if (showPopup) {
+        this.hide(immediate);
+      } else {
+        this.show(immediate);
+      }
+    },
+    hide(immediate = false) {
+      const { showPopup } = this.data;
+      if (showPopup == false) {
+        return;
+      }
+      this.setData({
+        transition: !immediate,
+        showPopup: false,
+      });
+      this.rerender();
+    },
+    show(immediate = false) {
+      const { showPopup } = this.data;
+      if (showPopup == true) {
+        return;
+      }
+      this.setData({
+        showPopup: true,
+        transition: !immediate,
+      });
       if (this.parent) {
-        this.parent.getChildWrapperStyle().then((res) => {
-          // console.log(res);
+        this.parent.getChildWrapperStyle().then((wrapperStyle) => {
           this.setData({
-            wrapperStyle: res,
+            wrapperStyle,
+            showWrapper: true,
           });
+          this.rerender();
+        });
+      }
+    },
+    updateDataFromParent() {
+      if (this.parent) {
+        const {
+          activeColor,
+          duration,
+          direction,
+          mask,
+          closeOnClickMask,
+        } = this.parent.properties;
+        this.setData({
+          activeColor,
+          duration,
+          direction,
+          mask,
+          closeOnClickMask,
         });
       }
     },
