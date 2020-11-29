60c167f37e3212f9d6f26991d0ff5c4678ec60ac
c10342 <1376023740@qq.com>
2020-11-29 20:03:46 +0800

    feat：完成 Toast 轻提示 组件

--- packages/Toast/toast.js
+++ packages/Toast/toast.js
@@ -1,87 +1,86 @@
-import { delete } from 'vue/types/umd'
-import {isObj} from '../common/utils'
+import { isObj } from "../common/utils";
 
 const defaultOptions = {
-    type:'text',
-    mask:false,
-    show:true,
-    zIndex:1000,
-    duration:2000,
-    position:'middle',
-    forbidClick:false,
-    selector:'#lin-toast'
+  type: "text",
+  mask: false,
+  show: true,
+  zIndex: 1000,
+  duration: 2000,
+  position: "middle",
+  forbidClick: false,
+  selector: "#lin-toast",
+};
+
+let queue = [];
+
+let currentOptions = { ...defaultOptions };
+
+function parseOptions(message) {
+  return isObj(message) ? message : { message };
 }
 
-let queue = []
-
-let currentOptions = {...defaultOptions}
-
-function parseOptions(message){
-    return isObj(message)?message:{message}
+function getContext() {
+  const pages = getCurrentPages();
+  return pages[pages.length - 1];
 }
 
-function getContext(){
-    const pages  = getCurrentPages()
-    return pages[pages.length-1]
-}
+function Toast(options) {
+  options = { ...currentOptions, ...parseOptions(options) };
 
-function Toast(options){
-    options = {...currentOptions,parseOptions(options)}
+  const context = options.context || getContext();
+  const toast = context.selectComponent(options.selector);
 
-    const context = options.context || getContext()
-    const toast = context.selectComponent(options.selector)
-
-    if(!toast){
-        console.warn('未找到 lin-toast 节点，请确认 selector 及 context 是否正确');
+  if (!toast) {
+    console.warn("未找到 lin-toast 节点，请确认 selector 及 context 是否正确");
     return;
-    }
+  }
 
-    delete options.context;
-    delete options.selector;
+  delete options.context;
+  delete options.selector;
 
-    toast.clear = ()=>{
-        toast.setData({show:false})
-        if(options.onClose){
-            options.onClose()
-        }
+  toast.clear = () => {
+    toast.setData({ show: false });
+    if (options.onClose) {
+      options.onClose();
     }
+  };
 
-    queue.push(toast)
-    toast.setData(options)
-    clearTimeout(toast.timer)
-    if(options.duration>0){
-        toast.timer = setTimeout(() => {
-            toast.clear()
-            queue = queue.filter(item=>item!==toast)
-        }, options.duration);
-    }
-    return toast
+  queue.push(toast);
+  toast.setData(options);
+  clearTimeout(toast.timer);
+  if (options.duration > 0) {
+    toast.timer = setTimeout(() => {
+      toast.clear();
+      queue = queue.filter((item) => item !== toast);
+    }, options.duration);
+  }
+  return toast;
 }
 
-const createMethod = type=>options=>{
-    Toast({
-        type,
-        ...parseOptions(options)
-    })
-}
+const createMethod = (type) => (options) => {
+  return Toast({
+    type,
+    ...parseOptions(options),
+  });
+};
 
-Toast.loading = createMethod('loading')
-Toast.success=createMethod('success')
-Toast.fail = createMethod('fail')
+Toast.loading = createMethod("loading");
+Toast.success = createMethod("success");
+Toast.fail = createMethod("fail");
 
-Toast.clear = ()=>{
-    queue.forEach(toast=>{
-        toast.clear()
-    })
-    queue = []
-}
+Toast.clear = () => {
+  queue.forEach((toast) => {
+    toast.clear();
+  });
+  queue = [];
+};
 
 Toast.setDefaultOptions = (options) => {
-    Object.assign(currentOptions, options);
-  };
-  
-  Toast.resetDefaultOptions = () => {
-    currentOptions = { ...defaultOptions };
-  };
+  Object.assign(currentOptions, options);
+};
 
-  export default Toast
+Toast.resetDefaultOptions = () => {
+  currentOptions = { ...defaultOptions };
+};
+
+export default Toast;
