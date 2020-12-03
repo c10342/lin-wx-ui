880d1fa4fc852cc463eabe43d23a85121f444102
c10342 <1376023740@qq.com>
2020-12-03 22:43:34 +0800

    feat：添加NoticeBar通告栏组件

---
+++ packages/NoticeBar/index.js
@@ -0,0 +1,104 @@
+Component({
+  options: {
+    addGlobalClass: true,
+    multipleSlots: true,
+  },
+  externalClasses: [
+    "custom-class",
+    "left-icon-class",
+    "wrapper-class",
+    "content-class",
+    "close-icon-class",
+    "navigator-class",
+  ],
+  properties: {
+    mode: {
+      type: String,
+      options: ["closeable", "link"],
+    },
+    text: {
+      type: String,
+      observer() {
+        wx.nextTick(() => {
+          this.updateStyle();
+        });
+      },
+    },
+    color: String,
+    background: String,
+    leftIcon: String,
+    delay: {
+      type: Number,
+      value: "0s",
+      observer() {
+        wx.nextTick(() => {
+          this.updateStyle();
+        });
+      },
+    },
+    speed: {
+      type: Number,
+      value: 50,
+      observer() {
+        wx.nextTick(() => {
+          this.updateStyle();
+        });
+      },
+    },
+    scrollable: {
+      type: Boolean,
+      value: true,
+    },
+    wrapable: Boolean,
+    openType: {
+      type: String,
+      value: "navigate",
+    },
+    mode: String,
+    url: String,
+  },
+  data: {
+    contentStyle: "",
+    show: true,
+  },
+  methods: {
+    updateStyle() {
+      const { scrollable, speed, delay } = this.properties;
+      if (scrollable) {
+        const query = this.createSelectorQuery();
+        query.select("#bar-content").boundingClientRect();
+        query.select("#bar-wrapper").boundingClientRect();
+        query.exec((rect) => {
+          const contentWidth = rect[0].width || 0;
+          const wrapperWidth = rect[1].width || 0;
+          const duration = Math.floor(contentWidth / speed);
+          const contentStyle = `transform: translateX(${Math.ceil(
+            wrapperWidth
+          )}px);animation-duration:${duration}s;animation-delay:${delay};`;
+          if (contentStyle !== this.data.contentStyle) {
+            this.setData({ contentStyle });
+          }
+        });
+      } else {
+        const contentStyle = "animation: none;";
+        if (contentStyle !== this.data.contentStyle) {
+          this.setData({ contentStyle });
+        }
+      }
+    },
+    onClick(event) {
+      this.triggerEvent("click", event);
+    },
+    onClickIcon(event) {
+      if (this.properties.mode === "closeable") {
+        this.setData({ show: false });
+        this.triggerEvent("close", event);
+      }
+    },
+  },
+  created: function() {},
+  attached: function() {},
+  ready: function() {},
+  moved: function() {},
+  detached: function() {},
+});
