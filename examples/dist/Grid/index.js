545161ded9c1548a0b75236bb4f744acbcf39fd9
c10342 <1376023740@qq.com>
2020-12-15 20:59:16 +0800

    feat：更新演示小程序

--- examples/dist/Grid/index.js
+++ examples/dist/Grid/index.js
@@ -19,46 +19,35 @@ Component({
   },
   externalClasses: ["custom-class"],
   properties: {
-    // 1
     columnNum: {
       type: Number,
       value: 4,
       observer: "updateChildren",
     },
-    // 1
     iconSize: {
       type: [String, Number],
       value: "56rpx",
       observer: "updateChildren",
     },
-    // 1
     gutter: {
       type: [String, Number],
       value: 0,
       observer: "updateChildren",
     },
-    // 1
     border: {
       type: Boolean,
       value: true,
       observer: "updateChildren",
     },
-    // 1
     center: {
       type: Boolean,
       value: true,
       observer: "updateChildren",
     },
-    // 1
     square: {
       type: Boolean,
       observer: "updateChildren",
     },
-    // clickable: {
-    //   type: Boolean,
-    //   observer: "updateChildren",
-    // },
-    // 1
     direction: {
       type: String,
       value: "vertical",
