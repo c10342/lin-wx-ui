11b45e0e34e4e8ecf214bd39a41f8a02f001aed6
c10342 <1376023740@qq.com>
2020-12-24 23:38:40 +0800

    feat：完善Area 省市区选择组件

--- packages/Area/index.js
+++ packages/Area/index.js
@@ -10,22 +10,62 @@ Component({
       value: {},
       observer: "updateColumns",
     },
+    value: {
+      type: String,
+      observer: "updateColumns",
+    },
+    title: String,
+    columnsNum: {
+      type: Number,
+      value: 3,
+      observer: "updateColumns",
+    },
+    columnsPlaceholder: {
+      type: Array,
+      value: [],
+      observer: "updateColumns",
+    },
+    loading: Boolean,
+    itemHeight: {
+      type: Number,
+      value: 44,
+    },
+    visibleItemCount: {
+      type: Number,
+      value: 6,
+    },
+    confirmButtonText: {
+      type: String,
+      value: "确定",
+    },
+    cancelButtonText: {
+      type: String,
+      value: "取消",
+    },
   },
   data: {
     columns: [],
   },
   methods: {
-    onConfirm() {},
-    onCancel() {},
+    onConfirm(event) {
+      this.triggerEvent("confirm", event.detail.value);
+    },
+    onCancel() {
+      this.triggerEvent("cancel");
+    },
     onChange(event) {
       const { index, picker } = event.detail;
       if (index === 0) {
         this.provinceChange(picker);
       } else if (index === 1) {
-        this.cityChange(picker);
+        this.cityChange(picker, index);
       }
     },
     provinceChange(picker) {
+      const { columnsNum } = this.properties;
+      if (columnsNum < 2) {
+        return;
+      }
       const selectProvice = picker.getColumnValue(0);
       const cityList = this.getCity(selectProvice);
       picker.setColumnValues(1, cityList);
@@ -33,27 +73,70 @@ Component({
       picker.setColumnIndex(1, 0);
     },
     cityChange(picker) {
+      const { columnsNum } = this.properties;
+      if (columnsNum < 3) {
+        return;
+      }
       const selectCity = picker.getColumnValue(1);
       const countyList = this.getCounty(selectCity);
       picker.setColumnValues(2, countyList);
       picker.setColumnIndex(2, 0);
     },
     updateColumns() {
+      const { columnsNum, value } = this.properties;
+      let provinceCode = "";
+      let cityCode = "";
+      let countyCode = "";
+      if (value) {
+        provinceCode = value.substring(0, 2);
+        cityCode = value.substring(0, 4);
+        countyCode = value.substring(0, 6);
+      }
       const columns = [];
-      const provinceList = this.getProvince();
-      columns.push({
-        values: provinceList,
-      });
-      if (provinceList.length > 0) {
-        const cityList = this.getCity(provinceList[0]);
-        columns.push({
-          values: cityList,
-        });
-        if (cityList.length > 0) {
-          const countyList = this.getCounty(cityList[0]);
-          columns.push({
-            values: countyList,
+      if (columnsNum >= 1) {
+        const provinceList = this.getProvince();
+        let provinceIndex = 0;
+        if (provinceCode) {
+          provinceIndex = provinceList.findIndex((item) => {
+            const code = (item.code || "").toString().substring(0, 2);
+            return provinceCode === code;
           });
+          provinceIndex = provinceIndex > -1 ? provinceIndex : 0;
+        }
+        columns.push({
+          values: provinceList,
+          defaultIndex: provinceIndex,
+        });
+        if (columnsNum >= 2) {
+          const cityList = this.getCity(provinceList[provinceIndex]);
+          let cityIndex = 0;
+          if (cityCode) {
+            cityIndex = cityList.findIndex((item) => {
+              const code = (item.code || "").toString().substring(0, 4);
+              return cityCode === code;
+            });
+            cityIndex = cityIndex > -1 ? cityIndex : 0;
+          }
+          columns.push({
+            values: cityList,
+            defaultIndex: cityIndex,
+          });
+          if (columnsNum >= 3) {
+            const countyList = this.getCounty(cityList[cityIndex]);
+            let countyIndex = 0;
+            if (countyCode) {
+              countyIndex = countyList.findIndex((item) => {
+                const code = (item.code || "").toString().substring(0, 6);
+                return countyCode === code;
+              });
+              countyIndex = countyIndex > -1 ? countyIndex : 0;
+            }
+            console.log(provinceIndex, cityIndex, countyIndex);
+            columns.push({
+              values: countyList,
+              defaultIndex: countyIndex,
+            });
+          }
         }
       }
       this.setData({
@@ -61,9 +144,15 @@ Component({
       });
     },
     getProvince() {
-      const { areaList = {} } = this.properties;
+      const { areaList = {}, columnsPlaceholder = [] } = this.properties;
       const { province_list = {} } = areaList;
       const provinceList = [];
+      if (columnsPlaceholder[0]) {
+        provinceList.push({
+          text: columnsPlaceholder[0],
+          code: -1,
+        });
+      }
       for (const key in province_list) {
         provinceList.push({
           text: province_list[key],
@@ -73,10 +162,19 @@ Component({
       return provinceList;
     },
     getCity(province) {
-      const code = province.code.substring(0, 2);
-      const { areaList = {} } = this.properties;
+      const { areaList = {}, columnsPlaceholder = [] } = this.properties;
       const { city_list = {} } = areaList;
       const cityList = [];
+      if (columnsPlaceholder[1]) {
+        cityList.push({
+          text: columnsPlaceholder[1],
+          code: -1,
+        });
+      }
+      if (!province || (province && province.code === -1)) {
+        return cityList;
+      }
+      const code = province.code.substring(0, 2);
       for (const key in city_list) {
         if (key.substring(0, 2) === code) {
           cityList.push({
@@ -88,10 +186,20 @@ Component({
       return cityList;
     },
     getCounty(city) {
-      const code = city.code.substring(0, 4);
-      const { areaList = {} } = this.properties;
+      const { areaList = {}, columnsPlaceholder = [] } = this.properties;
       const { county_list = {} } = areaList;
       const countyList = [];
+      if (columnsPlaceholder[2]) {
+        countyList.push({
+          text: columnsPlaceholder[2],
+          code: -1,
+        });
+      }
+      if (!city || (city && city.code === -1)) {
+        return countyList;
+      }
+
+      const code = city.code.substring(0, 4);
       for (const key in county_list) {
         if (key.substring(0, 4) === code) {
           countyList.push({
