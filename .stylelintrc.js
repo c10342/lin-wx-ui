432f81bb33ef887c0ed0bc1b63e402ba159b36d3
c10342 <1376023740@qq.com>
2021-01-04 23:21:44 +0800

    feat：配置stylelint文件

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,21 @@
 module.exports = {
-  extends: ["stylelint-config-standard","stylelint-config-recommended-scss", "stylelint-config-recess-order"],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recess-order"
+  ],
   plugins: ["stylelint-scss"],
   rules: {
     "no-descending-specificity": null,
-    'no-empty-source':null
+    "no-empty-source": null,
+    "unit-no-unknown": [true, { ignoreUnits: ["rpx"] }],
+    'selector-type-no-unknown': [
+      true,
+      {ignoreTypes:["lin-goods-action-button"]}
+    ],
+    'font-family-no-missing-generic-family-keyword': [
+      true,
+      {ignoreFontFamilies:['iconfont']}
+    ]
   }
 };
