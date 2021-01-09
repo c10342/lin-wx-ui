b38b681a05093fbc8cd20120cd66e0314c4014af
c10342 <1376023740@qq.com>
2021-01-09 19:52:58 +0800

    feat(waterflow&waterflowitem): 添加WaterFlow 瀑布流组件

---
+++ packages/WaterFlow/index.js
@@ -0,0 +1,101 @@
+Component({
+  options: {
+    addGlobalClass: true,
+    multipleSlots: true
+  },
+  relations: {
+    '../WaterFlowItem/index': {
+      type: 'descendant',
+      linked (child) {
+        this.children = this.children || [];
+        this.children.push(child);
+        this.renderWaterFlow();
+      },
+      unlinked (child) {
+        this.children = (this.children || []).filter(it => it !== child);
+        this.renderWaterFlow();
+      }
+    }
+  },
+  externalClasses: ['custom-class'],
+  properties: {
+    watchData: {
+      type: Array,
+      value: [],
+      observer: 'renderWaterFlow'
+    },
+    verticalMargin: {
+      type: Number,
+      value: 20,
+      observer: 'renderWaterFlow'
+    },
+    horizontalMargin: {
+      type: Number,
+      value: 10,
+      observer: 'renderWaterFlow'
+    }
+  },
+  data: {
+    height: 0
+  },
+  methods: {
+    renderWaterFlow () {
+      this.getContainer().then(container => {
+        this.setChildWidth(container);
+        this.setChildrenPosition();
+      });
+    },
+    setChildWidth (parentContainer) {
+      const { horizontalMargin } = this.properties;
+      const width = parentContainer.width / 2 - horizontalMargin;
+      (this.children || []).forEach(child => {
+        child.setWidth(width);
+      });
+    },
+    setChildrenPosition () {
+      wx.nextTick(() => {
+        const { verticalMargin, horizontalMargin } = this.properties;
+        let leftHeight = 0;
+        let rightHeight = 0;
+        const tasks = [];
+        (this.children || []).forEach(child => {
+          tasks.push(child.getRect());
+        });
+        Promise.all(tasks).then(result => {
+          result.forEach((res, index) => {
+            const child = this.children[index];
+            if (leftHeight <= rightHeight) {
+              child.setPosition(
+                `left:${horizontalMargin / 2}px;top:${leftHeight}px;`
+              );
+              leftHeight += res.height + verticalMargin * 1;
+            } else {
+              child.setPosition(
+                `right:${horizontalMargin / 2}px;top:${rightHeight}px;`
+              );
+              rightHeight += res.height + verticalMargin * 1;
+            }
+          });
+          this.setData({
+            height:
+              Math.ceil(Math.max(leftHeight, rightHeight)) - verticalMargin * 1
+          });
+        });
+      });
+    },
+    getContainer () {
+      return new Promise(resolve => {
+        const query = this.createSelectorQuery();
+        query
+          .select('.lin-water-flow')
+          .boundingClientRect(resolve)
+          .exec();
+      });
+    }
+  },
+  created () {},
+  attached () {},
+  ready () {},
+  moved () {},
+  detached () {}
+});
