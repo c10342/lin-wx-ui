57225acf430a70ed1239434e75e91f8d28d148f2
c10342 <1376023740@qq.com>
2021-01-29 20:55:00 +0800

    docs(examples): 更新演示小程序

--- examples/pages/navigator/logs/index.js
+++ examples/pages/navigator/logs/index.js
@@ -1,11 +1,14 @@
 import list from './logs.js'
+
+import {logsType} from '../../../config/index.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list:list
+    list:list,
+    addComponent:logsType.ADDCOMPONENT
   },
 
   /**
--- examples/pages/navigator/logs/logs.js
+++ examples/pages/navigator/logs/logs.js
@@ -1,153 +1,83 @@
-export default [{
-    time: '2020-12-30',
-    messageList: [
-      '新增 SwipeCell 滑动单元格组件'
+import { logsType } from "../../../config/index";
+
+export default [
+  {
+    version: "1.1.1",
+    time: "2021-01-29",
+    content: [
+      {
+        title: "新增组件",
+        list: [
+          "新增 Form 表单组件",
+          "新增 WaterFlow 瀑布流组件",
+          "新增 Area 省市区选择组件",
+          "新增 Card 商品卡片组件",
+          "新增 SubmitBar 提交订单栏组件",
+          "新增 GoodsAction 商品导航组件",
+        ],
+        type: logsType.ADDCOMPONENT
+      }
     ]
   },
   {
-    time: '2020-12-23',
-    messageList: [
-      '新增 Toast 轻提示组件'
+    version: "1.1.0",
+    time: "2020-12-28",
+    content: [
+      {
+        title: "新增组件",
+        list: [
+          "新增 Button 按钮组件",
+          "新增 Icon 图标组件",
+          "新增 Image 图片组件",
+          "新增 Loading 加载组件",
+          "新增 Layout 布局组件",
+          "新增 Cell 单元格组件",
+          "新增 Transition 动画组件",
+          "新增 Popup 弹出层组件",
+
+          "新增 Calendar 日历组件",
+          "新增 Checkbox 复选框组件",
+          "新增 Field 输入框组件",
+          "新增 Radio 单选框组件",
+          "新增 Rate 评分组件",
+          "新增 Search 搜索组件",
+          "新增 Slider 滑块组件",
+          "新增 Stepper 步进器组件",
+          "新增 Switch 开关组件",
+          "新增 Picker 选择器组件",
+          "新增 Uploader 文件上传组件",
+
+          "新增 Mask 遮罩层组件",
+          "新增 ActionSheet 上拉菜单组件",
+          "新增 Dialog 弹出框组件",
+          "新增 DropdownMenu 下拉菜单组件",
+          "新增 Notify 消息提示组件",
+          "新增 ShareSheet 分享面板组件",
+          "新增 Toast 轻提示组件",
+          "新增 SwipeCell 滑动单元格组件",
+
+          "新增 Collapse 折叠面板组件",
+          "新增 CountDown 倒计时组件",
+          "新增 Divider 分割线组件",
+          "新增 Empty 空状态组件",
+          "新增 NoticeBar 通告栏组件",
+          "新增 Panel 面板组件",
+          "新增 Progress 进度条组件",
+          "新增 Skeleton 骨架屏组件",
+          "新增 Steps 步骤条组件",
+          "新增 Sticky 粘性布局组件",
+          "新增 Tag 标签组件",
+          "新增 TreeSelect 分类选择组件",
+
+          "新增 Grid 宫格组件",
+          "新增 IndexBar 索引栏组件",
+          "新增 Sidebar 侧边导航组件",
+          "新增 NavBar 导航栏组件",
+          "新增 Tab 标签页组件",
+          "新增 Tabbar 标签栏组件",
+        ],
+        type: logsType.ADDCOMPONENT
+      }
     ]
-  },
-  {
-    time: '2020-12-19',
-    messageList: [
-      '新增 ShareSheet 分享面板组件'
-    ]
-  },
-  {
-    time: '2020-12-15',
-    messageList: [
-      '新增 Notify 消息提示组件'
-    ]
-  },
-  {
-    time: '2020-12-12',
-    messageList: [
-      '新增 DropdownMenu 下拉菜单组件'
-    ]
-  },
-  {
-    time: '2020-12-06',
-    messageList: [
-      '新增 Dialog 弹出框组件'
-    ]
-  },
-  {
-    time: '2020-11-26',
-    messageList: [
-      '新增 ActionSheet 上拉菜单组件'
-    ]
-  },
-  {
-    time: '2020-11-22',
-    messageList: [
-      '新增 Uploader 文件上传组件'
-    ]
-  },
-  {
-    time: '2020-11-19',
-    messageList: [
-      '新增 Picker 选择器组件'
-    ]
-  },
-  {
-    time: '2020-11-17',
-    messageList: [
-      '新增 Switch 开关组件'
-    ]
-  },
-  {
-    time: '2020-11-15',
-    messageList: [
-      '新增 Stepper 步进器组件'
-    ]
-  },
-  {
-    time: '2020-11-12',
-    messageList: [
-      '新增 Slider 滑块组件'
-    ]
-  },
-  {
-    time: '2020-11-09',
-    messageList: [
-      '新增 Search 搜索组件'
-    ]
-  },
-  {
-    time: '2020-11-07',
-    messageList: [
-      '新增 Rate 评分组件'
-    ]
-  },
-  {
-    time: '2020-11-05',
-    messageList: [
-      '新增 Radio 单选框组件'
-    ]
-  },
-  {
-    time: '2020-10-29',
-    messageList: [
-      '新增 Field 输入框组件'
-    ]
-  },
-  {
-    time: '2020-10-25',
-    messageList: [
-      '新增 Checkbox 复选框组件'
-    ]
-  },
-  {
-    time: '2020-10-24',
-    messageList: [
-      '新增 Calendar 日历组件'
-    ]
-  },
-  {
-    time: '2020-10-21',
-    messageList: [
-      '新增 Popup 弹出层组件'
-    ]
-  },
-  {
-    time: '2020-10-18',
-    messageList: [
-      '新增 Cell 单元格组件'
-    ]
-  },
-  {
-    time: '2020-10-13',
-    messageList: [
-      '新增 Loading 加载组件'
-    ]
-  },
-  {
-    time: '2020-10-10',
-    messageList: [
-      '新增 Layout 布局组件'
-    ]
-  },
-  {
-    time: '2020-10-06',
-    messageList: [
-      '新增 Mask 遮罩层组件',
-      '新增 Transition 动画组件'
-    ]
-  },
-  {
-    time: '2020-10-03',
-    messageList: [
-      '新增 Image 图片组件'
-    ]
-  },
-  {
-    time: '2020-10-01',
-    messageList: [
-      '新增 Button 按钮组件',
-      '新增 Icon 图标组件'
-    ]
-  },
-]
+  }
+];
