a7ed464c6377ab0bafd1e0d60fe646e9fd7e5e26
c10342 <1376023740@qq.com>
2021-02-05 11:07:30 +0800

    docs(examples): 更新演示小程序

--- examples/pages/component/uploader/index.js
+++ examples/pages/component/uploader/index.js
@@ -8,13 +8,13 @@ Page({
     fileList2: [
 
       {
-        url: 'https://img.yzcdn.cn/vant/leaf.jpg',
+        url: '/images/cat.png',
         name: '图片1',
       },
       // Uploader 根据文件后缀来判断是否为图片文件
       // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
       {
-        url: 'http://iph.href.lu/60x60?text=default',
+        url: '/images/cat.png',
         name: '图片2',
         isImage: true,
         deletable: true,
@@ -23,12 +23,12 @@ Page({
     fileList3:[],
     fileList4: [
       {
-        url: 'https://img.yzcdn.cn/vant/leaf.jpg',
+        url: '/images/cat.png',
         status: 'uploading',
         message: '上传中',
       },
       {
-        url: 'https://img.yzcdn.cn/vant/tree.jpg',
+        url: '/images/cat.png',
         status: 'failed',
         message: '上传失败',
       },
