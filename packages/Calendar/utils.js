7249cffa42fbeaedd0b13784ec51aff392470704
c10342 <1376023740@qq.com>
2021-03-22 16:34:10 +0800

    style(calendar): calendar组件添加代码注释说明

--- packages/Calendar/utils.js
+++ packages/Calendar/utils.js
@@ -26,15 +26,17 @@ export const getDate = (year, month, day) => {
   return new Date(year, month - 1, day);
 };
 
+// 对比2个时间
 const compareDate = (time1, time2, type) => {
   const date1 = new Date(time1);
   const date2 = new Date(time2);
-
+  // 传入的时间虽然可能是同一天的，但是可能会有时间差，所以需要统一一下
+  // toLocaleDateString() 方法返回该日期对象日期部分的字符串，该字符串格式因不同语言而不同
   const newDate1 = new Date(
-    date1.toLocaleDateString()
+    date1.toLocaleDateString('zh')
   );
   const newDate2 = new Date(
-    date2.toLocaleDateString()
+    date2.toLocaleDateString('zh')
   );
   const obj = {
     Equal: newDate1.getTime() === newDate2.getTime(),
@@ -46,13 +48,22 @@ const compareDate = (time1, time2, type) => {
   return obj[type];
 };
 
+// time1是否等于time2
 export const isEqual = (time1, time2) => compareDate(time1, time2, 'Equal');
 
+// time1是否小于等于time2
 export const isEqAndLt = (time1, time2) => compareDate(time1, time2, 'EqAndLt');
+
+// time1是否大于等于time2
 export const isEqAndGt = (time1, time2) => compareDate(time1, time2, 'EqAndGt');
+
+// time1是否小于time2
 export const isLt = (time1, time2) => compareDate(time1, time2, 'Lt');
+
+// time1是否大于time2
 export const isGt = (time1, time2) => compareDate(time1, time2, 'Gt');
 
+// 判断是否为当前月
 export const isCurrentMonth = (time, currentTime) => {
   const date1 = new Date(time);
   const date2 = new Date(currentTime);
