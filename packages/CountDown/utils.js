64ebb6bcb0a447c66745e0cd51de47328100e6bf
c10342 <1376023740@qq.com>
2021-03-22 19:39:41 +0800

    style(count-down): count-down组件添加代码注释说明

--- packages/CountDown/utils.js
+++ packages/CountDown/utils.js
@@ -1,3 +1,5 @@
+
+// 长度不足这前面补0
 function padZero (num, len = 2) {
   let str = `${num}`;
   while (str.length < len) {
@@ -11,7 +13,7 @@ export function isSameSecond (time1, time2) {
   return Math.floor(time1 / 1000) === Math.floor(time2 / 1000);
 }
 
-// 根据毫秒数转化成时分秒
+// 根据毫秒数转化成时分秒对象
 const SECOND = 1000;
 const MINUTE = 60 * SECOND;
 const HOUR = 60 * MINUTE;
@@ -32,30 +34,45 @@ export function parseTimeDate (time) {
   };
 }
 
+/**
+ * 根据timeDate转化成时间文本
+ * @param {*} format 需要格式化的格式
+ * @param {*} timeDate 包含时分秒毫秒的的对象
+ * @returns
+ */
 export function parseFormat (format, timeDate) {
   const { days } = timeDate;
+  // 获取对应的时分秒毫秒
   let {
     hours, minutes, seconds, milliseconds
   } = timeDate;
 
   if (format.indexOf('DD') > -1) {
+    // DD存在则说明需要格式化天
     format = format.replace('DD', padZero(days));
   } else {
+    // 不存在DD则需要，在小时这里加上天的时间
+    // 比如 1天1小时，不显示天则是25小时
     hours += days * 24;
   }
 
   if (format.indexOf('HH') > -1) {
+    // HH存在则说明需要格式化小时
     format = format.replace('HH', padZero(hours));
   } else {
+    // HH不存在，就是不显示小时，分钟需要加上小时的时间
+    // 比如1小时20分钟，不显示小时就是80分钟
     minutes += hours * 60;
   }
 
+  // 分钟
   if (format.indexOf('mm') > -1) {
     format = format.replace('mm', padZero(minutes));
   } else {
     seconds += minutes * 60;
   }
 
+  // 秒
   if (format.indexOf('ss') > -1) {
     format = format.replace('ss', padZero(seconds));
   } else {
