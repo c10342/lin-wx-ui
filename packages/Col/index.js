af89b5d06ee4877f67d437a16872a009ebdab93f
c10342 <1376023740@qq.com>
2021-03-22 16:38:11 +0800

    style(col): col组件添加代码注释说明

--- packages/Col/index.js
+++ packages/Col/index.js
@@ -10,14 +10,18 @@ Component({
     }
   },
   properties: {
+    // 列元素宽度
     span: Number,
+    // 列元素偏移距离
     offset: Number
   },
   data: {
+    // 根节点样式
     viewStyle: ''
   },
   methods: {
     setGutter (gutter) {
+      // 设置左右padding边距
       const padding = `${gutter / 2}px`;
       const viewStyle = gutter
         ? `padding-left: ${padding}; padding-right: ${padding};`
