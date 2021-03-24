42947d0d706b3e6f2de501b139f252e992966bc5
c10342 <1376023740@qq.com>
2021-03-24 19:43:21 +0800

    style(goods-action-icon): goods-action-icon组件添加注释说明

--- packages/GoodsActionIcon/index.js
+++ packages/GoodsActionIcon/index.js
@@ -3,27 +3,37 @@ import OpenTypeBehavior from '../behaviors/open-type';
 import ButtonBehavior from '../behaviors/button';
 
 Component({
+  name: 'GoodsActionIcon',
   options: {
     addGlobalClass: true,
     multipleSlots: true
   },
-  externalClasses: ['custom-class', 'icon-class', 'text-class'],
   behaviors: [LinkBehavior, OpenTypeBehavior, ButtonBehavior],
+  externalClasses: ['custom-class', 'icon-class', 'text-class'],
   properties: {
+    // 按钮文字
     text: String,
+    // 图标名称
     icon: String,
+    // 图标右上角提示信息
     info: {
       type: [String, Number]
     },
+    // 是否显示右上角小红点
     dot: Boolean,
+    // 点击后跳转的链接地址
     url: String,
+    // 是否显示为加载状态
     loading: Boolean,
+    // 是否禁用按钮
     disabled: Boolean
   },
   data: {},
   methods: {
+    // 点击组件
     onClick () {
       this.triggerEvent('click');
+      // 跳转页面
       const { url } = this.properties;
       this.jump(url);
     }
