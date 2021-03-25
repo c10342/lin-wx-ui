584bb5099799416b3ede5b47408c91ceb16f9caa
c10342 <1376023740@qq.com>
2021-03-25 15:22:22 +0800

    style(picker): picker组件添加注释说明

--- packages/Picker/index.js
+++ packages/Picker/index.js
@@ -1,4 +1,5 @@
 Component({
+  name: 'Picker',
   options: {
     addGlobalClass: true,
     multipleSlots: true
@@ -10,91 +11,119 @@ Component({
     'column-class'
   ],
   properties: {
+    // 对象数组，配置每一列显示的数据
     columns: {
       type: Array,
       value: []
     },
+    // 选项高度
     itemHeight: {
       type: Number,
       value: 44
     },
-    valueKey: {
+    // 选项对象中，文字对应的 key
+    textKey: {
       type: String,
       value: 'text'
     },
+    // 是否显示顶部栏
     showToolbar: Boolean,
+    // 顶部栏标题
     title: String,
+    // 确认按钮文字
     confirmButtonText: {
       type: String,
       value: '确认'
     },
+    // 取消按钮文字
     cancelButtonText: {
       type: String,
       value: '取消'
     },
+    // 顶部栏位置
     toolbarPosition: {
       type: String,
       value: 'top',
       options: ['top', 'bottom']
     },
+    // 可见的选项个数
     visibleItemCount: {
       type: Number,
       value: 6,
       observer: 'updateTopVisible'
     },
+    // 单列选择器的默认选中项索引
     defaultIndex: {
       type: Number,
       value: 0
     },
+    // 是否显示加载状态
     loading: Boolean
   },
   data: {
+    // 顶部可见个数
     topVisible: 2
   },
   methods: {
+    // 判断是否为简单列，非对象数组，简单列只有一列
     isSimple () {
       const { columns } = this.properties;
       return columns.length && !columns[0].values;
     },
+    // 更新顶部可见个数
     updateTopVisible () {
       const { visibleItemCount } = this.properties;
+      // 顶部可见个数为可见选项个数的一半
       const topVisible = parseInt(visibleItemCount / 2, 10) - 1;
       this.setData({
         topVisible
       });
     },
+    // 列值发生变化
     onChange (event) {
       wx.nextTick(() => {
         if (this.isSimple()) {
+          // 简单列，即只有单列
           this.triggerEvent('change', {
             picker: this,
             value: this.getColumnValue(0),
             index: this.getColumnIndex(0)
           });
         } else {
+          // 多列
           this.triggerEvent('change', {
             picker: this,
+            // 所有列选中的值
             value: this.getValues(),
+            // 第几列发生变化
             index: event.currentTarget.dataset.index
           });
         }
       });
     },
+    // 点击取消按钮
     onCancel () {
       this.emitByType('cancel');
     },
+    // 点击确定按钮
     onConfirm () {
       this.emitByType('confirm');
     },
     emitByType (type) {
       if (this.isSimple()) {
+        // 简单列，即单列
         this.triggerEvent(type, {
+          // 选中的值
           value: this.getColumnValue(0),
+          // 选中的索引值
           index: this.getColumnIndex(0)
         });
       } else {
+        // 多列
         this.triggerEvent(type, {
+          // 所有列选中的值
           value: this.getValues(),
+          // 所有列选中的索引
           index: this.getIndexes()
         });
       }
@@ -109,6 +138,7 @@ Component({
       const column = this.getColumn(index);
       return column ? column.data.currentIndex : '';
     },
+    // 获取对应的子组件，存在index则返回对应的下标值组件，否则就是全部
     getColumn (index) {
       const children = this.selectAllComponents('.lin-picker-column');
       if (index == null || index === undefined) {
@@ -128,20 +158,25 @@ Component({
     },
     // 设置对应列中所有选项
     setColumnValues (index, options) {
+      // 获取对应的列
       const column = this.getColumn(index);
       if (column == null) {
         return Promise.reject(new Error('setColumnValues: 对应列不存在'));
       }
+      // 对比传出的数据列表跟已经存在的数据列表是否相等
       const isSame = JSON.stringify(column.data.optionsList) === JSON.stringify(options);
       if (isSame) {
+        // 相同则什么都不做
         return Promise.resolve();
       }
       return new Promise((resolve) => {
+        // 设置列数据
         column.setData(
           {
             optionsList: options
           },
           () => {
+            // 设置完成之后，设置选中值
             column.setIndex(0);
             column.updateTranslateY();
             resolve();
@@ -188,6 +223,7 @@ Component({
   created () {},
   attached () {},
   ready () {
+    // 更新顶部可见个数
     this.updateTopVisible();
   },
   moved () {},
