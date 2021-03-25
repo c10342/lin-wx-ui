a0dc70133dffc9e270d8fdb71e25f6b0b2f3d89b
c10342 <1376023740@qq.com>
2021-03-25 15:25:16 +0800

    style(popup): popup组件添加注释说明

--- packages/Popup/index.js
+++ packages/Popup/index.js
@@ -2,76 +2,94 @@ import TransitionBehavior from '../behaviors/transition';
 import { getSystemInfoSync } from '../common/utils';
 
 Component({
+  name: 'Popup',
   options: {
     addGlobalClass: true
   },
-  externalClasses: ['custom-class'],
   behaviors: [TransitionBehavior(false)],
+  externalClasses: ['custom-class'],
   properties: {
+    // z-index 层级
     zIndex: {
       type: Number,
       value: 100
     },
+    // 是否显示遮罩层
     mask: {
       type: Boolean,
       value: true
     },
+    // 自定义遮罩层样式
     maskStyle: String,
+    // 弹出位置
     position: {
       type: String,
       value: 'center',
       observer: 'observeClass',
       options: ['top', 'bottom', 'right', 'left', 'center']
     },
+    // 是否显示关闭图标
     closeable: {
       type: Boolean,
       value: false
     },
+    // 关闭图标名称
     closeIcon: { type: String, value: 'close' },
+    // 关闭图标的位置
     closeIconPosition: {
       type: String,
       value: 'top-right',
       options: ['top-right', 'top-left', 'bottom-right', 'bottom-left']
     },
+    // 是否在点击遮罩层后关闭
     closeOnClickMask: {
       type: Boolean,
       value: true
     },
+    // 关闭图标大小
     closeIconSize: {
       type: [String, Number],
       value: '40rpx'
     },
+    // 是否显示圆角
     round: {
       type: Boolean,
       value: false
     },
+    // 动画类型，优先级高于 position
     transition: {
       type: String,
       observer: 'observeClass'
     },
+    // 是否为 iPhoneX 留出底部安全距离
     safeAreaInsetBottom: {
       type: Boolean,
       value: true
     },
+    // 是否留出顶部安全距离（状态栏高度）
     safeAreaInsetTop: {
       type: Boolean,
       value: false
     }
   },
   data: {
+    // 状态栏高度
     statusBarHeight: '0px'
   },
   methods: {
+    // 点击遮罩层
     onMaskClick () {
       this.triggerEvent('mask-click');
       if (this.properties.closeOnClickMask) {
         this.triggerEvent('close');
       }
     },
+    // 更新动画名称
     observeClass () {
       const { position, transition } = this.properties;
       this.setData({ name: transition || position });
     },
+    // 点击关闭按钮
     onCloseClick () {
       this.triggerEvent('close');
     }
@@ -81,6 +99,7 @@ Component({
   },
   attached () {},
   ready () {
+    // 获取状态栏高度
     const { statusBarHeight } = getSystemInfoSync();
     this.setData({
       statusBarHeight: `${statusBarHeight}px`
