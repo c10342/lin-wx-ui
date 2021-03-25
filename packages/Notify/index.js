9d981d9050ea49a2ed2ac192f56e1fb550bb0c74
c10342 <1376023740@qq.com>
2021-03-25 11:35:43 +0800

    style(notify): notify组件添加注释说明

--- packages/Notify/index.js
+++ packages/Notify/index.js
@@ -2,35 +2,44 @@ import { WHITE } from '../common/color';
 import { getSystemInfoSync } from '../common/utils';
 
 Component({
+  name: 'Notify',
   options: {
     addGlobalClass: true,
     multipleSlots: true
   },
   externalClasses: ['custom-class'],
   properties: {
+    // 类型
     type: {
       type: String,
       value: 'danger',
       options: ['success', 'primary', 'warning', 'danger', 'info']
     },
+    // 展示文案
     message: String,
+    // 展示时长(ms)，值为 0 时，notify 不会消失
     duration: {
       type: Number,
       value: 3000
     },
+    // 字体颜色
     color: {
       type: String,
       value: WHITE
     },
+    // 顶部距离
     top: {
       type: [String, Number],
       value: '0px'
     },
+    // 背景颜色
     background: String,
+    // z-index 层级
     zIndex: {
       type: Number,
       value: 100
     },
+    // 是否留出顶部安全距离（状态栏高度）
     safeAreaInsetTop: {
       type: Boolean,
       value: false
@@ -38,43 +47,57 @@ Component({
   },
 
   data: {
+    // 是否显示
     show: false,
+    // 点击的回调函数
     onClick: null,
+    // 打开的回调函数
     onOpened: null,
+    // 关闭的回调函数
     oClose: null,
+    // 状态栏高度
     statusBarHeight: '0px'
   },
   methods: {
+    // 显示组件
     show () {
       const { duration } = this.properties;
       const { onOpened } = this.data;
-      if (this.timer) {
-        clearTimeout(this.timer);
-      }
+      this.clearTimer();
       this.setData({ show: true });
       if (onOpened) {
+        // 执行打开回调函数
         wx.nextTick(onOpened);
       }
       if (duration > 0 && duration !== Infinity) {
+        // duration大于0或者duration不是无穷大
+        // 则自动消失
         this.timer = setTimeout(() => {
           this.hide();
         }, duration);
       }
     },
+    // 隐藏组件
     hide () {
       const { oClose } = this.data;
-      if (this.timer) {
-        clearTimeout(this.timer);
-      }
+      this.clearTimer();
       this.setData({ show: false });
-
       if (oClose) {
+        // 执行关闭回调函数
         wx.nextTick(oClose);
       }
     },
+    // 清空定时器
+    clearTimer () {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+    },
+    // 点击组件
     onTap (event) {
       const { onClick } = this.data;
       if (onClick) {
+        // 执行点击回调函数
         onClick(event.detail);
       }
     }
@@ -83,6 +106,7 @@ Component({
   attached () {},
   ready () {
     this.timer = null;
+    // 获取状态栏高度
     const { statusBarHeight } = getSystemInfoSync();
     this.setData({
       statusBarHeight: `${statusBarHeight}px`
@@ -90,8 +114,6 @@ Component({
   },
   moved () {},
   detached () {
-    if (this.timer) {
-      clearTimeout(this.timer);
-    }
+    this.clearTimer();
   }
 });
--- packages/Notify/notify.js
+++ packages/Notify/notify.js
@@ -1,43 +1,62 @@
 import { WHITE } from '../common/color';
 
+// 默认配置
 const defaultOptions = {
+  // 自定义节点选择器
   selector: '#lin-notify',
+  // 类型
   type: 'danger',
+  // 展示文案
   message: '',
+  // 背景颜色
   background: '',
+  // 展示时长(ms)，值为 0 时，notify 不会消失
   duration: 3000,
+  // z-index 层级
   zIndex: 100,
+  // 顶部距离
   top: '0px',
+  // 字体颜色
   color: WHITE,
+  // 是否留出顶部安全距离（状态栏高度）
   safeAreaInsetTop: false,
+  // 点击时的回调函数
   onClick: () => {},
+  // 完全展示后的回调函数
   onOpened: () => {},
+  // 关闭时的回调函数
   oClose: () => {}
 };
 
+// 当前配置
 let currentOptions = { ...defaultOptions };
 
+// 转化参数
 function parseOptions (option) {
   if (!option) {
     return {};
   }
   if (typeof option === 'string') {
+    // 如果是字符串类型，则需要转化为object类型
     return { message: option };
   }
   return option;
 }
 
+// 获取当前页面上下文
 function getContext () {
   const pages = getCurrentPages();
   return pages[pages.length - 1];
 }
 
 function Notify (options) {
+  // 合并参数
   options = { ...currentOptions, ...parseOptions(options) };
-
+  // 获取页面上下文
   const context = options.context || getContext();
+  // 获取组件
   const notify = context.selectComponent(options.selector);
-
+  // 删除上下文和节点选择器
   delete options.context;
   delete options.selector;
 
@@ -49,6 +68,7 @@ function Notify (options) {
   console.warn('未找到 lin-notify 节点，请确认 selector 及 context 是否正确');
 }
 
+// 关闭组件
 Notify.clear = function (options) {
   options = { ...currentOptions, ...parseOptions(options) };
   const context = options.context || getContext();
@@ -58,15 +78,19 @@ Notify.clear = function (options) {
   }
 };
 
+// 挂在当前配置
 Notify.currentOptions = currentOptions;
 
+// 挂在默认配置
 Notify.defaultOptions = defaultOptions;
 
+// 设置当前配置
 Notify.setDefaultOptions = (options) => {
   currentOptions = { ...currentOptions, ...options };
   Notify.currentOptions = currentOptions;
 };
 
+// 重置当前配置为默认配置
 Notify.resetDefaultOptions = () => {
   currentOptions = { ...defaultOptions };
   Notify.currentOptions = currentOptions;
