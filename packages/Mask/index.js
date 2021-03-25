dbbc1dc7239f963c3bb460d58ffaa89855a63b74
c10342 <1376023740@qq.com>
2021-03-25 11:33:54 +0800

    style(mask): mask组件添加注释说明

--- packages/Mask/index.js
+++ packages/Mask/index.js
@@ -1,23 +1,29 @@
 // Component Object
 Component({
+  name: 'Mask',
   options: {
     addGlobalClass: true
   },
   externalClasses: ['custom-class'],
   properties: {
+    // 是否展示遮罩层
     show: {
       type: Boolean,
       value: false
     },
+    // 动画时长，单位秒
     duration: {
       type: null,
       value: 200
     },
+    // z-index 层级
     zIndex: {
       type: Number,
       value: 100
     },
+    // 自定义样式
     customStyle: String,
+    // 透明度
     opacity: {
       type: Number,
       value: 0.5
@@ -25,6 +31,7 @@ Component({
   },
   data: {},
   methods: {
+    // 点击遮罩层
     onClick () {
       this.triggerEvent('click');
     }
