9dbd8977cbabb4a6594b335bcb9ab8e562a1d1c7
c10342 <1376023740@qq.com>
2021-03-26 17:51:11 +0800

    style(steps): steps组件添加注释说明

--- packages/Steps/index.js
+++ packages/Steps/index.js
@@ -1,4 +1,5 @@
 Component({
+  name: 'Steps',
   options: {
     addGlobalClass: true,
     multipleSlots: true
@@ -11,25 +12,32 @@ Component({
     'line-class'
   ],
   properties: {
+    // 步骤
     steps: {
       type: Array,
       value: []
     },
+    // 当前步骤
     active: {
       type: Number,
       value: 0
     },
+    // 显示方向
     direction: {
       type: String,
       value: 'horizontal',
       options: ['horizontal', 'vertical']
     },
+    // 激活状态颜色
     activeColor: String,
+    // 未激活状态颜色
     inactiveColor: String,
+    // 激活状态底部图标
     activeIcon: {
       type: String,
       value: 'round-active'
     },
+    // 未激活状态底部图标
     inactiveIcon: String
   },
   data: {},
