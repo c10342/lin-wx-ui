9777e459cada3afee27033e4b7aa128ad7e3c3e1
c10342 <1376023740@qq.com>
2021-03-26 15:50:09 +0800

    style(share-sheet): share-sheet组件添加注释说明

--- packages/ShareSheet/options.js
+++ packages/ShareSheet/options.js
@@ -5,14 +5,17 @@ Component({
   },
   externalClasses: ['custom-class', 'options-class', 'option-class', 'button-class', 'image-class', 'icon-class', 'name-class', 'description-class'],
   properties: {
+    // 分享选项
     options: {
       type: Array,
       value: []
     },
+    // 是否显示边框
     showBorder: Boolean
   },
   data: {},
   methods: {
+    // 点击选项
     onSelect (event) {
       const { index } = event.currentTarget.dataset;
       const option = this.properties.options[index];
