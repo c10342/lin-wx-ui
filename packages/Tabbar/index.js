8b11365614d6f124913fb3908f4f921d5476b8e2
c10342 <1376023740@qq.com>
2021-03-29 19:50:32 +0800

    style(tabbar): tabbar组件添加注释说明

--- packages/Tabbar/index.js
+++ packages/Tabbar/index.js
@@ -1,10 +1,12 @@
 import { getRect } from '../common/utils';
 
 Component({
+  name: 'Tabbar',
   options: {
     addGlobalClass: true,
     multipleSlots: true,
   },
+  externalClasses: ['custom-class', 'placeholder-class'],
   relations: {
     '../TabbarItem/index': {
       type: 'descendant',
@@ -25,8 +27,8 @@ Component({
       },
     },
   },
-  externalClasses: ['custom-class', 'placeholder-class'],
   properties: {
+    // 当前选中标签的索引
     active: {
       type: [String, Number],
       value: 0,
@@ -34,43 +36,54 @@ Component({
         this.updateCurrentIndex();
       },
     },
+    // 是否固定在底部
     fixed: {
       type: Boolean,
       value: true,
       observer: 'initPlaceholderView',
     },
+    // 固定在底部时，是否在标签位置生成一个等高的占位元素
     placeholder: {
       type: Boolean,
       observer: 'initPlaceholderView',
     },
+    // 是否展示外边框
     border: {
       type: Boolean,
       value: true,
     },
+    // 元素 z-index
     zIndex: Number,
+    // 选中标签的颜色
     activeColor: {
       type: String,
       observer: 'updateChildren',
     },
+    // 未选中标签的颜色
     inactiveColor: {
       type: String,
       observer: 'updateChildren',
     },
+    // 是否为 iPhoneX 留出底部安全距离
     safeAreaInsetBottom: {
       type: Boolean,
       value: true,
     },
   },
   data: {
+    // 当前激活的索引
     currentIndex: 0,
+    // 组件高度，用于生成等高的占位元素
     height: 0,
   },
   methods: {
+    // 更新TabbarItem子组件
     updateChildren() {
       (this.children || []).forEach((child) => {
         child.updateFromParent();
       });
     },
+    // 更新选中的索引号
     updateCurrentIndex() {
       const { active } = this.properties;
       (this.children || []).forEach((child, index) => {
@@ -79,12 +92,14 @@ Component({
             currentIndex: index,
           });
         }
+        // 设置组件状态（是否选中状态）
         child.setActive(active === child.getComponentName());
       });
     },
     emitChange(name) {
       this.triggerEvent('change', name);
     },
+    // 生成等高占位元素
     initPlaceholderView() {
       const { placeholder, fixed } = this.properties;
       if (placeholder && fixed) {
