437cdbdcdc355f51684aa7e5bb38d90565ce6fbe
c10342 <1376023740@qq.com>
2021-03-29 19:51:01 +0800

    style(tabbar-item): tabbar-item组件添加注释说明

--- packages/TabbarItem/index.js
+++ packages/TabbarItem/index.js
@@ -1,8 +1,10 @@
 Component({
+  name: 'TabbarItem',
   options: {
     addGlobalClass: true,
     multipleSlots: true,
   },
+  externalClasses: ['custom-class', 'content-class'],
   relations: {
     '../Tabbar/index': {
       type: 'ancestor',
@@ -15,24 +17,32 @@ Component({
       },
     },
   },
-  externalClasses: ['custom-class', 'content-class'],
   properties: {
+    // 标签名称，作为匹配的标识符
     name: {
       type: [String, Number],
     },
+    // 图标名称
     icon: String,
+    // 是否显示小红点
     dot: Boolean,
+    // 图标右上角提示信息
     info: {
       type: [String, Number],
     },
   },
   data: {
+    // 组件宽度
     width: 0,
+    // 是否选中状态
     active: false,
+    // 选中标签的颜色
     activeColor: '',
+    // 未选中标签的颜色
     inactiveColor: '',
   },
   methods: {
+    // 获取组件唯一标识
     getComponentName() {
       const { name } = this.properties;
       if (name != null) {
@@ -40,6 +50,7 @@ Component({
       }
       return this.index;
     },
+    // 从父组件中获取数据并更新
     updateFromParent() {
       if (this.parent) {
         const params = {};
@@ -52,6 +63,7 @@ Component({
         this.setDiffData(params);
       }
     },
+    // 找出不同的数据项，并更新
     setDiffData(data) {
       const obj = Object.keys(data).reduce((prev, current) => {
         if (this.data[current] !== data[current]) {
@@ -62,9 +74,11 @@ Component({
 
       this.setData(obj);
     },
+    // 设置状态（是否选中）
     setActive(active) {
       this.setData({ active });
     },
+    // 点击组件
     onClick() {
       if (this.data.active || !this.parent) {
         return;
