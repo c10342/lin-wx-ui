052b900981d4f126a6a8440ce162d09a2bb31669
c10342 <1376023740@qq.com>
2021-03-30 09:30:48 +0800

    style(water-flow-item): water-flow-item组件添加注释说明

--- packages/WaterFlowItem/index.js
+++ packages/WaterFlowItem/index.js
@@ -1,10 +1,12 @@
 import { getRect } from '../common/utils';
 
 Component({
+  name: 'WaterFlowItem',
   options: {
     addGlobalClass: true,
     multipleSlots: true,
   },
+  externalClasses: ['custom-class'],
   relations: {
     '../WaterFlow/index': {
       type: 'ancestor',
@@ -16,21 +18,25 @@ Component({
       },
     },
   },
-  externalClasses: ['custom-class'],
   properties: {},
   data: {
+    // 位置样式
     positionStyle: '',
+    // 组件宽度
     width: '50%',
   },
   methods: {
+    // 获取组件元素信息
     getRect() {
       return getRect(this, '.lin-water-flow-item');
     },
+    // 设置组件位置
     setPosition(positionStyle) {
       this.setData({
         positionStyle,
       });
     },
+    // 设置组件宽度
     setWidth(width) {
       this.setData({
         width: parseInt(width),
