7e29eef2c562218fc6c1a44dd1052a98fbef2c1f
c10342 <1376023740@qq.com>
2021-04-15 15:20:57 +0800

    test(utils.js): 完善测试工具类

--- tests/utils.js
+++ tests/utils.js
@@ -1,6 +1,17 @@
 import path from 'path';
 import simulate from 'miniprogram-simulate';
 
+export function loadTemplate(obj = {}) {
+  return render(simulate.load(obj));
+}
+
+export function mounted(compName, props = {}) {
+  // 重置，不然每次生成的时候会报错
+  jest.resetModules();
+  const id = getCompId(compName);
+  return render(id, props);
+}
+
 export function getCompId(compName) {
   return simulate.load(
     path.resolve(__dirname, `./../packages/${compName}/index`)
@@ -15,45 +26,67 @@ export function render(compId, props = {}) {
 }
 
 // 获取元素
-export function getElement(context, element) {
-  return new WXUtils(context, element);
+export function getElement(context, selector) {
+  return new CompUtils(context, selector);
 }
 
-class WXUtils {
-  constructor(context, element) {
+class CompUtils {
+  constructor(context, selector) {
     this.context = context;
-    this.element = element;
+    this.selector = selector;
     this.initDom();
   }
 
   initDom() {
-    this.dom = querySelector(this.context, this.element);
+    this.component = querySelector(this.context, this.selector);
   }
 
   getClassNames() {
     this.initDom();
-    return getAttribute(this.dom, 'class');
+    return getAttribute(this.component, 'class');
   }
 
   hasClassName(className) {
     this.initDom();
-    return hasClassName(this.dom, className);
+    return hasClassName(this.component, className);
   }
 
   getAttribute(attr) {
     this.initDom();
-    return getAttribute(this.dom, attr);
+    return getAttribute(this.component, attr);
   }
 
   exists() {
     this.initDom();
-    return !!this.dom;
+    return !!this.component;
+  }
+
+  async dispatchEvent(evnetName) {
+    this.initDom();
+    const fn = jest.fn();
+    this.context.instance.triggerEvent = fn;
+    await this.component.dispatchEvent(evnetName);
+    return fn;
+  }
+
+  getExternalClasses(externalClass) {
+    this.initDom();
+    return getExternalClasses(this.component, externalClass);
+  }
+
+  hasExternalClass(externalClass, className) {
+    this.initDom();
+    const classList = getExternalClasses(this.component, externalClass);
+    if (Array.isArray(classList)) {
+      return classList.includes(className);
+    }
+    return false;
   }
 }
 
 // 判断是否存在类名
-function hasClassName(element, classname) {
-  const classes = getAttribute(element, 'class');
+function hasClassName(component, classname) {
+  const classes = getAttribute(component, 'class');
   if (!classes) {
     return false;
   }
@@ -62,8 +95,8 @@ function hasClassName(element, classname) {
 }
 
 //  获取元素上面的属性
-function getAttribute(element, attr) {
-  const attrsArr = element.dom.__wxElement._vt.attrs;
+function getAttribute(component, attr) {
+  const attrsArr = component.dom.__wxElement._vt.attrs;
   const attrs = {};
   for (let i = 0; i < attrsArr.length; i++) {
     const attrItem = attrsArr[i];
@@ -72,6 +105,12 @@ function getAttribute(element, attr) {
   return attr ? attrs[attr] : attrs;
 }
 
-function querySelector(context, element) {
-  return context.querySelector(element);
+// 获取外部样式类
+function getExternalClasses(component, externalClass) {
+  const classesObj = component._exparserNode.__externalClassAlias;
+  return externalClass ? classesObj[externalClass] : classesObj;
+}
+
+export function querySelector(context, selector) {
+  return context.querySelector(selector);
 }
