62f88bce4ce81004c3493b1b83bdfba2782d8f1f
c10342 <1376023740@qq.com>
2021-05-23 15:04:09 +0800

    docs(storage): 更新Storage功能

---
+++ examples/dist/extends/storage/cache.js
@@ -0,0 +1,42 @@
+const cacheKey = '_storage_cache_key_';
+
+export function addCacheKey(key) {
+  const cacheList = wx.getStorageSync(cacheKey) || [];
+  const index = cacheList.findIndex((k) => key === k);
+  if (index > -1) {
+    cacheList.splice(index, 1);
+  }
+  cacheList.push(key);
+  wx.setStorageSync(cacheKey, cacheList);
+}
+
+export function deleteCacheKey(key) {
+  const cacheList = wx.getStorageSync(cacheKey) || [];
+  const index = cacheList.findIndex((k) => key === k);
+  if (index > -1) {
+    cacheList.splice(index, 1);
+  }
+  wx.setStorageSync(cacheKey, cacheList);
+}
+
+export function getCahceKey() {
+  return wx.getStorageSync(cacheKey) || [];
+}
+
+export function removeCacheKey(key) {
+  wx.removeStorageSync(key);
+  deleteCacheKey(key);
+}
+
+export function isOverLimitSize(maxSize) {
+  const info = wx.getStorageInfoSync();
+  let limitSize = info.limitSize;
+  if (maxSize && maxSize > 0 && maxSize <= info.limitSize) {
+    limitSize = maxSize;
+  }
+  if (info.currentSize >= limitSize) {
+    // 超出存储长度
+    const outKeys = getCahceKey() || [];
+    removeCacheKey(outKeys[0]);
+  }
+}
