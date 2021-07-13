9bc6ac8dce89ba43dd98a5be950fb01fbdbe8bf1
c10342 <1376023740@qq.com>
2021-07-13 19:48:14 +0800

    feat(is.js): 添加判断数据类型的公共方法

---
+++ packages/common/is.js
@@ -0,0 +1,21 @@
+const toString = Object.prototype.toString;
+
+export function isFunction(data) {
+  return toString.call(data) === '[object Function]';
+}
+
+export function isString(data) {
+  return toString.call(data) === '[object String]';
+}
+
+export function isNull(data) {
+  return toString.call(data) === '[object Null]';
+}
+
+export function isUndefined(data) {
+  return toString.call(data) === '[object Undefined]';
+}
+
+export function isUndef(data) {
+  return isUndefined(data) || isNull(data);
+}
